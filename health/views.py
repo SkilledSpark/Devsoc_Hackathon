dfe9cb902195d2095f22c1e4aafa57f1ff055c66
SkilledSpark <69134903+SkilledSpark@users.noreply.github.com>
2023-06-02 18:39:24 +0530

    Login page added
    Implemented login page and redirection to home page

---
+++ health/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+def form_submission(request):
+    if request.method == "POST":
+        # Retrieve the username and password from the form
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Perform any necessary processing or storage of the username and password
+
+        # Redirect to the analysis route
+        return HttpResponseRedirect("/health/home/")
+    else:
+        return render(request, 'health/login.html')
+
+def redirect(request):
+    return render(request, "health/home.html")
